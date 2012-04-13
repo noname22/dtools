c08bbd4334d2fd1b54e828a328f9819eccf7004c
Fredrik Hultin <noname@nurd.se>
2012-04-13 16:46:23 +0200

    started adding machine options for dinterpret. added frequency selection for dinterpret.

--- dinterpret/src/main.c
+++ dinterpret/src/main.c
@@ -1,8 +1,20 @@
 #include "common.h"
 #include "dcpu.h"
 #include "dinterpret.h"
+#include <SDL.h>
 
 int logLevel;
+
+typedef enum {
+	MT_None, MT_Notch, MT_Noname
+} MachineType;
+
+typedef struct {
+	MachineType machine;
+	int freq;
+	const char* file;
+	const char* debugFile;
+} Settings;
 
 void SysWrite(Dcpu* me, void* data)
 {
@@ -21,7 +33,7 @@ void SysRead(Dcpu* me, void* data)
 	while(*lbuffer) *buffer++ = *lbuffer++;
 }
 
-int Start(const char* file, const char* debugFile)
+int Start(Settings* settings)
 {
 	Dcpu* cpu = Dcpu_Create();
 	uint16_t* ram = Dcpu_GetRam(cpu);
@@ -29,25 +41,30 @@ int Start(const char* file, const char* debugFile)
 	Dcpu_SetSysCall(cpu, SysRead, 1, NULL);
 	Dcpu_SetSysCall(cpu, SysWrite, 2, NULL);
 
-	LoadRam(ram, file);
+	LoadRam(ram, settings->file);
 
 	Debug* debugger = NULL;
 
-	if(debugFile){
+	if(settings->debugFile){
 		debugger = Debug_Create(cpu);
-		Debug_LoadSymbols(debugger, debugFile);
+		Debug_LoadSymbols(debugger, settings->debugFile);
 	}
 	
 	LogV("Ram before execution:");
 	if(logLevel <= 1) DumpRam(ram, GetUsedRam(ram));
 
+	LogV("sleeping every %d instructions", settings->freq);
+
 	int stillRunning = 0;
-	while((stillRunning = Dcpu_Execute(cpu, 10)) || debugger){
+	while((stillRunning = Dcpu_Execute(cpu, settings->freq ? settings->freq : 1000)) || debugger){
 		if(debugger && !stillRunning){
 			LogI("program finished");
 			debugger->runInstructions = 0;
 			Debug_Inspector(cpu, debugger);
 		}
+
+		// Sleep 1 millisecond for every ~freq cycles executed (default 7000)
+		if(settings->freq) SDL_Delay(1);
 	}
 
 	int returnValue = Dcpu_GetRegister(cpu, DR_A);
@@ -65,12 +82,21 @@ int main(int argc, char** argv)
 {
 	const char* usage = "usage: %s (-vX | -d) [dcpu-16 binary]";
 	LAssert(argc >= 2, usage, argv[0]);
+
+	Settings settings;
+	memset(&settings, 0, sizeof(Settings));
+
+	settings.freq = 7000;
+
 	bool debugging = false;
 
 	logLevel = 2;
 	
-	const char* file = NULL;
 	int numFiles = 0;
+	float fFreq;
+
+	char machineStr[64] = {0};
+	strcat(machineStr, "none");
 
 	for(int i = 1; i < argc; i++){
 		char* v = argv[i];
@@ -81,29 +107,35 @@ int main(int argc, char** argv)
 				LogI("Available flags:");
 				LogI("  -vX   set log level, where X is [0-5] - default: 2");
 				LogI("  -d    start with debugger");
+				LogI("  -fF   interpret at frequency F in MHz - default 7.0 MHz, 0.0 = as fast as possible");
+				LogI("  -mM   start with machine M - none (default, only cpu), notch (speculative), noname (my own awesome machine)");
 				return 0;
 			}
+			else if(sscanf(v, "-f%f", &fFreq) == 1){ settings.freq = (int)(fFreq * 1000.0f); }
 			else if(sscanf(v, "-v%d", &logLevel) == 1){}
 			else if(!strcmp(v, "-d")){ debugging = true; }
+			else if(sscanf(v, "-m%s", machineStr)){}
 			else{
 				LogF("No such flag: %s", v);
 				return 1;
 			}
 		}else{
 			numFiles++;
-			file = v;
+			settings.file = v;
 		}
 	}
+
+	if(!strcmp(machineStr, "noname")){ settings.machine = MT_Noname; }
+	else if(!strcmp(machineStr, "notch")){ settings.machine = MT_Notch; }
+	else{ settings.machine = MT_None; }
 	
 	LAssert(numFiles == 1, "Please specify one file to run");
 
-	const char* debugFile = NULL;
 	if(debugging){	
 		char tmp[512] = {0};
-		strcat(tmp, file);
+		strcat(tmp, settings.file);
 		strcat(tmp, ".dbg");
-		debugFile = tmp;
+		settings.debugFile = tmp;
 	}
 
-	return Start(file, debugFile);
-}
+	return Start(&settings);}
